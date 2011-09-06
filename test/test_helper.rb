7471a2fe58288e5980b74ee3319a6c504cd2063c
Simon Chiang <simon.chiang@pinnacol.com>
2011-09-06 11:20:15 -0600

    sketched out gem test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -50,3 +50,80 @@ module ShellTest
     end
   end
 end
+
+# Helpers for testing linecook with various gem fixtures installed.
+module GemTestHelpers
+  FIXTURES_DIR = File.expand_path('../fixtures', __FILE__)
+
+  def gem_fixture_dir
+    path('fixtures')
+  end
+
+  def gem_install_dir
+    path('gems')
+  end
+
+  def gem_path(name, version, *relative_path)
+    File.join(gem_fixture_dir, "#{name}-#{version}", *relative_path)
+  end
+
+  def build_gem_fixture(name, version)
+    gemspec = prepare gem_path(name, version, "#{name}.gemspec") do |io|
+      io << Gem::Specification.new do |s|
+        s.name    = name
+        s.version = version
+        s.summary = "Summary #{name} #{version}"
+        s.require_paths = ["lib"]
+        yield(s)
+      end.to_ruby
+    end
+
+    Dir.chdir gem_path(name, version) do
+      cmd = "gem build '#{gemspec}' 2>&1"
+      stdout = `#{cmd}`
+
+      unless $?.exitstatus == 0
+        raise "failure to build gem:\n#{cmd}\n#{stdout}"
+      end
+
+      FileUtils.mv gem_path(name, version, "#{name}-#{version}.gem"), gem_fixture_dir
+    end
+  end
+
+  def prepare_gem_file(spec, path, content=nil, &block)
+    prepare(gem_path(spec.name, spec.version, path), content, &block)
+    spec.files << path
+  end
+
+  def install_gem_fixtures
+    gemfiles = Dir.glob File.join(gem_fixture_dir, '*.gem')
+    gemfiles.each do |gemfile|
+      cmd = "gem install '#{gemfile}' --local --no-rdoc --no-ri --install-dir '#{gem_install_dir}' 2>&1"
+      stdout = `#{cmd}`
+
+      unless $?.exitstatus == 0
+        raise "failure to install gem:\n#{cmd}\n#{stdout}"
+      end
+    end
+  end
+
+  def with_gem_env
+    # Bundler.with_clean_env do
+      current_gem_home = ENV['GEM_HOME']
+      current_gem_path = ENV['GEM_PATH']
+      begin
+        ENV['GEM_HOME'] = gem_install_dir
+        ENV['GEM_PATH'] = gem_install_dir
+        yield
+      rescue
+        ENV['GEM_HOME'] = current_gem_home
+        ENV['GEM_PATH'] = current_gem_path
+      end
+    # end
+  end
+
+  def with_gem_fixtures(&block)
+    install_gem_fixtures
+    with_gem_env(&block)
+  end
+end
