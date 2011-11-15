5c980273cfadf797bebab9bcb77d2e27cf9b03a3
Simon Chiang <simon.chiang@pinnacol.com>
2011-11-15 08:20:48 -0700

    get write(,ln,lit) tests to pass for document

--- lib/linecook/document.rb
+++ lib/linecook/document.rb
@@ -1,10 +1,25 @@
+require 'linecook/line'
+require 'strscan'
+
 module Linecook
   class Document
     # An array of lines in the document.
     attr_reader :lines
+    attr_accessor :eol
+    attr_accessor :indent
+    attr_accessor :rstrip
+    attr_accessor :lstrip
+    attr_reader :linebreak
+    attr_reader :logger
 
-    def initialize(lines=[])
+    def initialize(lines=[], logger=nil)
       @lines = lines
+      @eol = "\n"
+      @indent = ""
+      @rstrip = false
+      @lstrip = false
+      @linebreak = /\r?\n/
+      @logger = logger
     end
 
     # Returns the position of the content in lines, or nil if lines does not
@@ -16,6 +31,84 @@ module Linecook
       lines.rindex do |current|
         current.equal?(content)
       end
+    end
+
+    def linebreak=(regex)
+      @linebreak = Regexp === regex ? regex : Regexp.new(Regexp.escape(regex))
+    end
+
+    def buffer
+      buffer = lines.last
+
+      if String === buffer && !buffer.end_with?(eol)
+        lines.last
+      else
+        nil
+      end
+    end
+
+    def scan(str)
+      if logger
+        logger.debug "scan: #{str.inspect}"
+      end
+
+      if buffer
+        str = "#{lines.pop}#{str}"
+      end
+
+      scanner = StringScanner.new(str)
+      result  = nil
+
+      while line = scanner.scan_until(linebreak)
+        result = yield format(line)
+      end
+
+      unless scanner.eos?
+        result = yield format(scanner.rest)
+      end
+
+      result
+    end
+
+    def format(line)
+      if logger
+        logger.debug "format: #{line.inspect}"
+      end
+
+      unless line =~ linebreak
+        return line
+      end
+
+      line = $`
+      line.rstrip! if rstrip
+      line.lstrip! if lstrip
+
+      "#{indent}#{line}#{eol}"
+    end
+
+    def write(str)
+      scan(str) do |line|
+        writelit line
+      end
+    end
+
+    def writeln(str)
+      scan("#{str}#{eol}") do |line|
+        writelit line
+      end
+    end
+
+    def writelit(str)
+      if logger
+        logger.debug "writelit: #{str.inspect}"
+      end
+
+      lines << str
+      Line.new(str, self)
+    end
+
+    def to_s
+      lines.join
     end
   end
 end
--- test/linecook/document_test.rb
+++ test/linecook/document_test.rb
@@ -1,5 +1,6 @@
 require 'test/unit'
 require 'linecook/document'
+require 'logger'
 
 class DocumentTest < Test::Unit::TestCase
   Document = Linecook::Document
@@ -8,7 +9,9 @@ class DocumentTest < Test::Unit::TestCase
 
   def setup
     super
-    @doc = Document.new
+    logger = $DEBUG ? Logger.new(STDOUT) : nil
+    puts if logger
+    @doc = Document.new([], logger)
   end
 
   #
@@ -35,5 +38,147 @@ class DocumentTest < Test::Unit::TestCase
 
   def test_pos_returns_nil_if_lines_does_not_contain_content
     assert_equal nil, doc.pos("unknown")
+  end
+
+  #
+  # write test
+  #
+
+  def test_write_adds_str_to_doc
+    doc.write "line\n"
+    assert_equal "line\n", doc.to_s
+  end
+
+  def test_write_reformats_str_with_indent
+    doc.indent = '..'
+    doc.write "a\n"
+    doc.indent = '.'
+    doc.write "b\n"
+    doc.indent = ''
+    doc.write "c\n"
+
+    assert_equal "..a\n.b\nc\n", doc.to_s
+  end
+
+  def test_write_reformats_str_with_eol
+    doc.eol = "."
+    doc.write "a\n"
+    doc.write "b\n"
+    doc.write "c\n"
+
+    assert_equal "a.b.c.", doc.to_s
+  end
+
+  def test_write_continues_last_line_until_linebreak
+    doc.write "a"
+    doc.write "b"
+    doc.write "c\n"
+    doc.write "x"
+    doc.write "y"
+    doc.write "z\n"
+
+    assert_equal "abc\nxyz\n", doc.to_s
+  end
+
+  def test_write_rewrites_linebreak_to_eol
+    doc.linebreak = '.'
+    doc.eol = ';'
+
+    doc.write "a;"
+    doc.write "b;"
+    doc.write "c."
+    doc.write "x;"
+    doc.write "y;"
+    doc.write "z."
+
+    assert_equal "a;b;c;x;y;z;", doc.to_s
+  end
+
+  def test_write_rstrips_at_linebreak_if_specified
+    doc.rstrip = true
+    doc.write "a "
+    doc.write "b "
+    doc.write "c \nxyz \n"
+
+    assert_equal "a b c\nxyz\n", doc.to_s
+  end
+
+  def test_write_applies_indent_after_rstrip
+    doc.rstrip = true
+    doc.indent = "  "
+    doc.write "a "
+    doc.write "b "
+    doc.write "c \nxyz \n"
+
+    assert_equal "  a b c\n  xyz\n", doc.to_s
+  end
+
+  def test_write_lstrips_at_linebreak_if_specified
+    doc.lstrip = true
+    doc.write " a"
+    doc.write " b"
+    doc.write " c\n xyz\n"
+
+    assert_equal "a b c\nxyz\n", doc.to_s
+  end
+
+  def test_write_applies_indent_after_lstrip
+    doc.lstrip = true
+    doc.indent = "  "
+    doc.write " a"
+    doc.write " b"
+    doc.write " c\n xyz\n"
+
+    assert_equal "  a b c\n  xyz\n", doc.to_s
+  end
+
+  def test_write_reformats_multiple_lines
+    doc.indent = "  "
+    doc.eol    = "."
+    doc.linebreak = "\n"
+    doc.write "abc\nxyz\n"
+
+    assert_equal "  abc.  xyz.", doc.to_s
+  end
+
+  #
+  # writeln test
+  #
+
+  def test_writeln_adds_str_and_eol_to_doc
+    doc.eol = '.'
+    doc.writeln "line"
+    assert_equal "line.", doc.to_s
+  end
+
+  def test_writeln_accepts_multiple_input_lines
+    doc.writeln "abc\nxyz\n"
+    assert_equal "abc\nxyz\n\n", doc.to_s
+  end
+
+  def test_writeln_continues_last_line
+    doc.write "a"
+    doc.write "b"
+    doc.writeln "c"
+    doc.write "x"
+    doc.write "y"
+    doc.writeln "z"
+
+    assert_equal "abc\nxyz\n", doc.to_s
+  end
+
+  #
+  # writelit test
+  #
+
+  def test_writelit_writes_string_without_processing
+    doc.indent = ""
+    doc.writelit "abc\nxyz\n"
+    assert_equal "abc\nxyz\n", doc.to_s
+  end
+
+  def test_writelit_returns_line_for_full_literal
+    line = doc.writelit "abc\nxyz\n"
+    assert_equal "abc\nxyz\n", line.to_s
   end
 end
