8152f8b645bbbccfa9c1ed0258bbf6d2d234b7b2
Simon Chiang <simon.chiang@pinnacol.com>
2011-11-22 08:50:52 -0700

    sketch out linked list document model

--- lib/linecook/document.rb
+++ lib/linecook/document.rb
@@ -1,67 +1,99 @@
 require 'linecook/line'
-require 'linecook/section'
 require 'linecook/format'
 
 module Linecook
   class Document
-    attr_reader :lines
-    attr_reader :format
+    include Enumerable
 
-    def initialize(lines=[], format=nil)
-      @lines = lines
-      @format = (format || Format.new).freeze
+    def initialize(first=nil, last=nil)
+      @first = first
+      @last  = last
     end
 
-    # Sets format attributes (note this resets format to a new object).
-    def set(attrs)
-      @format = format.with(attrs).freeze
+    def first
+      # automatically correct if first has been prepended
+      @first ? @first = @first.first : nil
     end
 
-    # Sets format attributes for the duration of a block.
-    def with(attrs)
-      current = format
-      begin
-        set attrs
-        yield
-      ensure
-        @format = current
+    def last
+      # automatically correct if last has been appended
+      @last ? @last = @last.last : nil
+    end
+
+    def each
+      current = first
+      while current
+        yield current
+        current = current.nex
       end
+      self
+    end
+
+    def reverse_each
+      current = last
+      while current
+        yield current
+        current = current.pre
+      end
+      self
     end
 
     def pos(line)
-      include?(line) ? line.pos : nil
+      inject(0) do |pos, current|
+        return pos if current == line
+        pos + 1
+      end
     end
 
     def length
-      lines.inject(0) do |pos, line|
-        pos + line.length
+      count
+    end
+
+    def at(index)
+      if index < 0
+        index = length + index
+      end
+
+      inject(0) do |pos, line|
+        if pos == index
+          return line
+        end
+        pos + 1
+      end
+
+      nil
+    end
+
+    def next_line(str="")
+      if last.nil?
+        @first = @last = Line.new(str)
+      else
+        @last = last.next_line(str)
       end
     end
 
-    def include?(line)
-      lines.any? {|current| current.equal? line }
-    end
+    def next_section(str, *strs)
+      head, tail = next_line(str)
 
-    def prepend(*new_lines)
-      new_lines.reverse_each do |line|
-        line.insert_into lines, 0
+      while str = strs.shift
+        tail = tail.append(str)
       end
-    end
 
-    def append(*new_lines)
-      new_lines.each do |line|
-        line.insert_into lines, -1
-      end
+      @last = tail
+      head.down_to(tail)
     end
 
     def write(str)
-      line = Line.new(str, format)
-      append line
-      line
+      (last || next_line).write(str)
+    end
+
+    # Writes a line to self.
+    def writeln(str)
+      write "#{str}\n"
     end
 
     def insert(pos, str)
-      lines.inject(0) do |current_pos, line|
+      inject(0) do |current_pos, line|
         current_pos += line.length
 
         if current_pos > pos
@@ -76,33 +108,11 @@ module Linecook
       write(' ' * (pos - current_pos) + str)
     end
 
-    # Writes a line to self.
-    def writeln(str)
-      write "#{str}\n"
-    end
-
-    # Indents n levels for the duration of the block.
-    def indent(n=1)
-      with(:indent => n) do
-        yield
-      end
-    end
-
-    # Outdents for the duration of the block.  A negative number can be
-    # provided to outdent n levels.
-    def outdent(n=nil)
-      with(:indent => n) do
-        yield
-      end
-    end
-
     # Returns the formatted contents of self as a string.
-    def to_s
-      target = ""
-      lines.each do |line|
-        line.render_to target
+    def to_s(target="")
+      each do |line|
+        target << line.to_s
       end
-      target
     end
   end
 end
--- lib/linecook/line.rb
+++ lib/linecook/line.rb
@@ -1,59 +1,90 @@
 module Linecook
   class Line
-    attr_reader :content
-    attr_reader :lines
-    attr_reader :format
+    attr_reader :str
+    attr_accessor :pre
+    attr_accessor :nex
 
-    def initialize(content, lines=[], format=nil)
-      @content = content
-      @lines   = lines
-      @format  = format
+    def initialize(str, pre=nil, nex=nil, head=self, tail=self)
+      @str = str
+      @pre = pre
+      @nex = nex
+      @head = head
+      @tail = tail
+
+      pre.nex = self if pre
+      nex.pre = self if nex
     end
 
-    def rewrite(content)
-      @content = content
+    def write(str)
+      @str << str
     end
 
-    def pos
-      lines.inject(0) do |pos, current|
-        return pos if current.equal? self
-        pos + current.length
-      end
-      nil
+    def rewrite(str)
+      @str = str
     end
 
     def length
-      content.length
+      str.to_s.length
     end
 
-    def index
-      lines.index do |current|
-        current.equal? self
-      end
+    def first
+      first? ? self : pre.first
     end
 
-    def rindex
-      lines.rindex do |current|
-        current.equal? self
-      end
+    def first?
+      pre.nil?
     end
 
-    def prepend(*lines)
-      self.lines.insert(index || 0, *lines)
-      lines.last
+    def last
+      last? ? self : nex.last
     end
 
-    def append(*lines)
-      self.lines.insert((rindex || -1) + 1, *lines)
-      lines.last
+    def last?
+      nex.nil?
     end
 
-    def render_to(target)
-      if format
-        format.render(content, target)
+    def head
+      @head || (first? ? self : pre.head)
+    end
+
+    def tail
+      @tail || (last? ? self : nex.tail)
+    end
+
+    def next_line(str="")
+      current = tail # discover tail only once
+      Line.new(str, current, current.nex)
+    end
+
+    def prepend(str)
+      Line.new(str, pre, head)
+    end
+
+    def append(str)
+      line  = Line.new(str, tail, nex, nil)
+      @tail = nil
+      line
+    end
+
+    def rel(pos=0)
+      case 
+      when pos > 0
+        nex.at pos - 1
+      when pos < 0
+        pre.at pos + 1
       else
-        target << content
+        self
       end
+    end
+
+    def down_to(line=nil, lines=[])
+      lines << self
+      line == self || last? ? lines : nex.down_to(line, lines)
+    end
+
+    def up_to(line=nil, lines=[])
+      lines.unshift self
+      line == self || first? ? lines : pre.up_to(line, lines)
     end
   end
 end
